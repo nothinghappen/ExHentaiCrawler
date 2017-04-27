45389ad3b9fac7f88a4347231c7e3b66dfe818ca
nothinghappen <foxwj9516@foxmail.com>
2017-04-27 21:00:53 +0800

    数据库建表脚本

---
+++ database/eh.sql
@@ -0,0 +1,19 @@
+create table eromanga (
+	id int not null auto_increment,
+	gid int not null,
+    token varchar(20) not null,
+    archiver_key varchar(200) not null,
+    title varchar(300) not null,
+    title_jpn varchar(300) not null,
+    category varchar(20) not null,
+    thumb varchar(200) not null,
+    uploader varchar(50) not null,
+    posted long not null,
+    filecount int not null,
+    filesize long not null,
+    expunged boolean not null,
+    rating varchar(20) not null,
+    torrentcount int not null,
+    tags varchar(1000) not null,
+    primary key(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
